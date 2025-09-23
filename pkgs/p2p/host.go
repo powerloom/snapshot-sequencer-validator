3ff12982018b65587596a7a82ee15a308c82106d
anomit <anomit.ghosh@gmail.com>
2025-09-23 16:47:45 +0530

    feat: add connection manager to P2P host initialization

--- pkgs/p2p/host.go
+++ pkgs/p2p/host.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
@@ -12,7 +13,7 @@ import (
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/routing"
+	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
 	"github.com/multiformats/go-multiaddr"
 	"github.com/powerloom/snapshot-sequencer-validator/config"
 	log "github.com/sirupsen/logrus"
@@ -34,36 +35,40 @@ func NewP2PHost(ctx context.Context, cfg *config.Settings) (*P2PHost, error) {
 
 	p2pPort := strconv.Itoa(cfg.P2PPort)
 
-	// Build libp2p options
+	// Configure connection manager
+	connMgr, err := connmgr.NewConnManager(
+		cfg.ConnManagerLowWater,
+		cfg.ConnManagerHighWater,
+		connmgr.WithGracePeriod(time.Minute),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create connection manager: %w", err)
+	}
+	log.Infof("Connection manager configured: LowWater=%d, HighWater=%d", cfg.ConnManagerLowWater, cfg.ConnManagerHighWater)
+
+	// Build libp2p options (simplified, matching working implementation)
 	opts := []libp2p.Option{
 		libp2p.Identity(privKey),
-		libp2p.ListenAddrStrings(
-			fmt.Sprintf("/ip4/0.0.0.0/tcp/%s", p2pPort),
-			fmt.Sprintf("/ip6/::/tcp/%s", p2pPort),
-		),
-		libp2p.DefaultMuxers,
-		libp2p.DefaultTransports,
-		libp2p.DefaultSecurity,
-		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
-			return dht.New(ctx, h, dht.Mode(dht.ModeClient))
-		}),
-		libp2p.NATPortMap(),
-		libp2p.EnableAutoRelay(),
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%s", p2pPort)),
 		libp2p.EnableNATService(),
+		libp2p.ConnectionManager(connMgr),
 	}
 
 	// Add public IP if configured
 	if cfg.P2PPublicIP != "" {
 		publicAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%s", cfg.P2PPublicIP, p2pPort))
-		if err == nil {
+		if err != nil {
+			log.Errorf("Failed to create public multiaddr: %v", err)
+		} else {
 			opts = append(opts, libp2p.AddrsFactory(func(addrs []multiaddr.Multiaddr) []multiaddr.Multiaddr {
+				// Add the public address to the list
 				return append(addrs, publicAddr)
 			}))
 			log.Infof("Advertising public IP: %s", cfg.P2PPublicIP)
 		}
 	}
 
-	// Create libp2p host
+	// Create libp2p host with options
 	h, err := libp2p.New(opts...)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create host: %w", err)
